8b20e345ee829b48eb0bf5adbabfb0d6a0c79a47
Michael Petterson <michaelpetterson06@gmail.com>
2025-05-20 16:23:57 -0300

    Swegger.

--- cashforce-api/src/server.js
+++ cashforce-api/src/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const routes = require('./routes/order.routes');
+const setupSwagger = require('./swagger');
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
+setupSwagger(app);
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
---
+++ cashforce-api/src/swagger.js
@@ -0,0 +1,22 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Cashforce API',
+      version: '1.0.0',
+      description: 'Documentação da API do desafio Cashforce',
+    },
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const setupSwagger = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+module.exports = setupSwagger;
