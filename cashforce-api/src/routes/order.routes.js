98edf1c407516dca09d93f69afd3b3829936d6d1
Michael Petterson <michaelpetterson06@gmail.com>
2025-05-21 11:39:02 -0300

    Including a comment in swegger to make it work again and inserting logging: false to not pollute the back end with sequelize requests.

--- cashforce-api/src/routes/order.routes.js
+++ cashforce-api/src/routes/order.routes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const OrderController = require('../controllers/OrderController');
 
+/**
+ * @swagger
+ * /orders:
+ *   get:
+ *     summary: Lista todos os pedidos
+ *     responses:
+ *       200:
+ *         description: Lista de pedidos retornada com sucesso
+ */
 router.get('/orders', OrderController.findAll);
 
 module.exports = router;
