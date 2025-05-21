88ed323c60e71f4a3b8656b33dd077303a28617c
Michael Petterson <michaelpetterson06@gmail.com>
2025-05-21 15:20:26 -0300

    Responsiveness to differents screens.

--- frontend/src/components/OrderRow.vue
+++ frontend/src/components/OrderRow.vue
@@ -1,48 +1,73 @@
 <template>
-    <div class="grid grid-cols-6 items-center px-6 py-3 bg-white border border-[#DFE2EB] rounded-lg">
-      <div class="text-sm text-gray-700">{{ order.orderNfId }}</div>
-      <div class="text-sm text-gray-700">{{ order.buyer.name }}</div>
-      <div class="text-sm text-gray-700">{{ order.provider.name }}</div>
-      <div class="text-sm text-gray-700">{{ formatDate(order.emissionDate) }}</div>
-
-      <div class="text-sm font-semibold text-brand-green">R$ {{ formatCurrency(order.value) }}</div>
-
-      <div class="flex items-center justify-between">
-        <span class="text-sm font-semibold text-brand-green">
-          {{ order.statusDescription }}
-        </span>
-        <button class="whitespace-nowrap px-4 py-2  border border-blue-200 rounded-full text-xs font-medium text-gray-700 hover:bg-blue-50">
-          Dados do cedente
-        </button>
-      </div>
+  <div
+    class="px-6 py-3 bg-white border border-[#DFE2EB] rounded-lg lg:grid lg:grid-cols-6 lg:items-center flex flex-col gap-2"
+  >
+  
+    <div class="text-sm text-gray-700 flex lg:block gap-1">
+      <span class="font-semibold text-gray-500 lg:hidden">Nota Fiscal:</span>
+      {{ order.orderNfId }}
     </div>
-  </template>
-  
-  
-  <script>
-  export default {
-    props: {
-      order: Object
-    },
-    methods: {
-      formatCurrency(value) {
-        if (!value) return '0,00';
-        return Number(value).toLocaleString('pt-BR', {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2,
-        });
-       },
 
-      formatDate(date) {
-        if (!date) return '';
-        const d = new Date(date);
-        const day = String(d.getDate()).padStart(2, '0');
-        const month = String(d.getMonth() + 1).padStart(2, '0');
-        const year = d.getFullYear();
-        return `${day}/${month}/${year}`;
-        }
-    }
-  };
-  </script>
   
+    <div class="text-sm text-gray-700 flex lg:block gap-1">
+      <span class="font-semibold text-gray-500 lg:hidden">Sacado:</span>
+      {{ order.buyer.name }}
+    </div>
+
   
+    <div class="text-sm text-gray-700 flex lg:block gap-1">
+      <span class="font-semibold text-gray-500 lg:hidden">Cedente:</span>
+      {{ order.provider.name }}
+    </div>
+
+  
+    <div class="text-sm text-gray-700 flex lg:block gap-1">
+      <span class="font-semibold text-gray-500 lg:hidden">Emissão:</span>
+      {{ formatDate(order.emissionDate) }}
+    </div>
+
+  
+    <div class="text-sm font-semibold text-brand-green flex lg:block gap-1">
+      <span class="font-semibold text-gray-500 lg:hidden">Valor:</span>
+      R$ {{ formatCurrency(order.value) }}
+    </div>
+
+  
+    <div class="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-2">
+      <span class="text-sm font-semibold text-brand-green block text-start">
+        {{ order.statusDescription }}
+      </span>
+
+      <button
+        class="whitespace-nowrap px-4 py-2 border border-blue-200 rounded-full text-xs font-medium 	text-gray-700 hover:bg-blue-50"
+      >
+        Dados do cedente
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    order: Object,
+  },
+  methods: {
+    formatCurrency(value) {
+      if (!value) return '0,00';
+      return Number(value).toLocaleString('pt-BR', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+    },
+    formatDate(date) {
+      if (!date) return '';
+      const d = new Date(date);
+      const day = String(d.getDate()).padStart(2, '0');
+      const month = String(d.getMonth() + 1).padStart(2, '0');
+      const year = d.getFullYear();
+      return `${day}/${month}/${year}`;
+    },
+  },
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex h-screen bg-white">
 
-    <Sidebar />
+    <Sidebar class="hidden md:block" />
 
     <main class="flex-1 bg-white">
 
@@ -9,16 +9,19 @@
         <div class="h-[1px] w-full" style="background-color: #DFE2EB;"></div>
       </div>
 
-      <div class="pl-16 pr-8 pt-[40px]">
+
+      <div class="px-4 md:px-16 pt-[40px]">
         <div class="flex items-center gap-2 mb-1">
           <img src="/Vector-2.svg" alt="Ícone Notas" class="w-5 h-5" />
           <h1 class="text-2xl font-bold text-gray-800">Notas fiscais</h1>
         </div>
         <p class="text-gray-500 mb-6">Visualize as notas fiscais que você tem.</p>
+        <p class="text-red-500 text-sm mb-6 block md:hidden">
+          Para melhor visualização, acesse em um computador.
+        </p>
 
-        <div class="bg-white shadow rounded-lg overflow-hidden">
-          <div class="grid grid-cols-6 px-6 py-3 text-xs font-bold uppercase" style="color: #A1A8B8;">
-
+        <div class="bg-white shadow rounded-lg overflow-x-auto">
+          <div class="hidden lg:grid grid-cols-6 px-6 py-3 text-xs font-bold uppercase" style="color: #A1A8B8;">
             <div>Nota Fiscal</div>
             <div>Sacado</div>
             <div>Cedente</div>
@@ -27,37 +30,30 @@
             <div>Status</div>
           </div>
           <div class="space-y-3 mt-4">
-          <OrderRow
-            v-for="(order, index) in orders"
-            :key="index"
-            :order="order"
-          />
-        </div>
+            <OrderRow
+              v-for="(order, index) in orders"
+              :key="index"
+              :order="order"
+            />
+          </div>
         </div>
       </div>
     </main>
-
   </div>
 </template>
 
 <script>
-import axios from 'axios';
 import Sidebar from '../components/Sidebar.vue';
 import OrderRow from '../components/OrderRow.vue';
+import { getOrders } from '../services/orderService';
 
 export default {
-  components: {
-    Sidebar,
-    OrderRow
-  },
+  components: { Sidebar, OrderRow },
   data() {
-    return {
-      orders: []
-    };
+    return { orders: [] };
   },
   async mounted() {
-    const { data } = await axios.get('http://localhost:3000/orders');
-    this.orders = data;
-  }
+    this.orders = await getOrders();
+  },
 };
 </script>
