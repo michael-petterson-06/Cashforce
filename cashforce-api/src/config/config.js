818578830492b37b7667f7ef9aa9832c720a3582
Michael Petterson <michaelpetterson06@gmail.com>
2025-05-24 13:36:25 -0300

    Dotenv on the index.js file.

--- cashforce-api/src/config/config.js
+++ cashforce-api/src/config/config.js
@@ -1,46 +1,28 @@
 require('dotenv').config();
 
-// module.exports = {
-//   development: {
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: 'mysql'
-//   },
-//   test: {
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: 'cashforce_test',
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: 'mysql'
-//   },
-//   production: {
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: 'cashforce_prod',
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: 'mysql'
-//   }
-// };
-
-
-require('dotenv').config();
-
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
-    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
   },
   test: {
-    url: process.env.DATABASE_URL,
-    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'cashforce_test',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
   },
   production: {
-    url: process.env.DATABASE_URL,
-    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'cashforce_prod',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
   }
 };
