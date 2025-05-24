e4ff7f2580dbaab25d4642bdc0caefd75e2a1c3a
Michael Petterson <michaelpetterson06@gmail.com>
2025-05-24 15:30:02 -0300

    Simplify config.js for Railway.

--- cashforce-api/src/config/config.js
+++ cashforce-api/src/config/config.js
@@ -1,28 +1,37 @@
 require('dotenv').config();
-module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql'
-  },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'cashforce_test',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql'
-  },
-  production: {
-    username: process.env.DB_USER || 'log_vazio',
-    password: process.env.DB_PASSWORD || 'log_vazio',
-    database: process.env.DB_NAME || 'log_vazio',
-    host: process.env.DB_HOST || 'log_vazio',
-    port: process.env.DB_PORT || 3306,
-    dialect: 'mysql'
-  }
-};
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql'
+//   },
+//   test: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: 'cashforce_test',
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql'
+//   },
+//   production: {
+//     username: process.env.DB_USER || 'log_vazio',
+//     password: process.env.DB_PASSWORD || 'log_vazio',
+//     database: process.env.DB_NAME || 'log_vazio',
+//     host: process.env.DB_HOST || 'log_vazio',
+//     port: process.env.DB_PORT || 3306,
+//     dialect: 'mysql'
+//   }
+// };
 
+module.exports = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  logging: false
+};
