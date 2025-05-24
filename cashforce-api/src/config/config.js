b745ff677c9a51e3b5c0202779a6c07abe51443b
Michael Petterson <michaelpetterson06@gmail.com>
2025-05-24 14:42:35 -0300

    Force loading variables in index.js.

--- cashforce-api/src/config/config.js
+++ cashforce-api/src/config/config.js
@@ -18,11 +18,11 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.DB_USER || 'log_vazio',
+    password: process.env.DB_PASSWORD || 'log_vazio',
+    database: process.env.DB_NAME || 'log_vazio',
+    host: process.env.DB_HOST || 'log_vazio',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql'
   }
 };
