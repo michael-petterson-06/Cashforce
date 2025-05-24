0ab584753af3060ab74496448c70a04fb9eb1194
Michael Petterson <michaelpetterson06@gmail.com>
2025-05-24 20:14:55 -0300

    Add Procfile to fix port on Railway.

--- cashforce-api/src/index.js
+++ cashforce-api/src/index.js
@@ -1,13 +1,17 @@
-require('dotenv').config();
-const app = require('./server');
+// require('dotenv').config();
+// const app = require('./server');
 
 // const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => {
 //   console.log(`Servidor rodando na porta ${PORT}`);
 // });
 
+const app = require('./server');
 
+// Railway SEMPRE fornece process.env.PORT
+// fallback 3000 só funciona localmente
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
